902164133ef61bf609aad1f7de5e4f0f20170a0f
Guillaume Grossetie <ggrossetie@gmail.com>
2021-06-25 10:03:06 +0200

    Do not add EXPLAIN if query already starts with EXPLAIN or PROFILE

--- test/runner.spec.js
+++ test/runner.spec.js
@@ -90,4 +90,88 @@ CREATE CONSTRAINT ON (v:Venue) ASSERT v.name IS UNIQUE;`,
       getSessionStub.restore()
     }
   })
+  it('should not add EXPLAIN if the query already starts with EXPLAIN', async () => {
+    const connectStub = sinon.stub(neo4jSession, 'connect')
+    const executeStub = sinon.stub(neo4jSession, 'execute')
+    const getSessionStub = sinon.stub(neo4jSession, 'getSession')
+    try {
+      connectStub.returns(Promise.resolve({}))
+      executeStub.returns(Promise.resolve({}))
+      getSessionStub.returns({
+        close: sinon.fake()
+      })
+      const reporter = new Reporter()
+      const cypherQueriesCatalog = [{
+        queries: [
+          {
+            content: `EXPLAIN CREATE CONSTRAINT ON (a:Article) ASSERT a.index IS UNIQUE`,
+            sourceLocation: {
+              lineNumber: 5,
+              path: '/path/to/foo.adoc'
+            }
+          }
+        ]
+      }]
+      await runQueriesFromCatalog(cypherQueriesCatalog, {
+        neo4jDriver: {},
+        reporter
+      })
+      const calls = executeStub.getCalls()
+      expect(calls.length).to.equal(1)
+      expect(calls[0].args[1]).to.equal('EXPLAIN CREATE CONSTRAINT ON (a:Article) ASSERT a.index IS UNIQUE')
+    } finally {
+      connectStub.restore()
+      executeStub.restore()
+      getSessionStub.restore()
+    }
+  })
+  it('should not add EXPLAIN if the query starts with PROFILE', async () => {
+    const connectStub = sinon.stub(neo4jSession, 'connect')
+    const executeStub = sinon.stub(neo4jSession, 'execute')
+    const getSessionStub = sinon.stub(neo4jSession, 'getSession')
+    try {
+      connectStub.returns(Promise.resolve({}))
+      executeStub.returns(Promise.resolve({}))
+      getSessionStub.returns({
+        close: sinon.fake()
+      })
+      const reporter = new Reporter()
+      const cypherQueriesCatalog = [{
+        queries: [
+          {
+            content: `PROFILE
+MATCH (p1:Person), (p2:Person)
+WHERE p1 <> p2
+MATCH path = (p1)-[*..5]-(p2)
+WITH p1, p2, path
+ORDER BY p1, p2, length(path) DESC
+WITH p1, p2, collect(path)[0] AS path
+RETURN p1.name, p2.name, [rel in relationships(path) | type(rel)]`,
+            sourceLocation: {
+              lineNumber: 5,
+              path: '/path/to/foo.adoc'
+            }
+          }
+        ]
+      }]
+      await runQueriesFromCatalog(cypherQueriesCatalog, {
+        neo4jDriver: {},
+        reporter
+      })
+      const calls = executeStub.getCalls()
+      expect(calls.length).to.equal(1)
+      expect(calls[0].args[1]).to.equal(`PROFILE
+MATCH (p1:Person), (p2:Person)
+WHERE p1 <> p2
+MATCH path = (p1)-[*..5]-(p2)
+WITH p1, p2, path
+ORDER BY p1, p2, length(path) DESC
+WITH p1, p2, collect(path)[0] AS path
+RETURN p1.name, p2.name, [rel in relationships(path) | type(rel)]`)
+    } finally {
+      connectStub.restore()
+      executeStub.restore()
+      getSessionStub.restore()
+    }
+  })
 })
